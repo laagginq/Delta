0afaabc15b2956cc02263fb39a75b17fe3feae6c
xz <95953670+laagginq@users.noreply.github.com>
2024-09-17 19:40:00 -0700

    Create loader.lua

---
+++ loader.lua
@@ -0,0 +1,7 @@
+local GameID = game.GameId
+
+if GameID == 3647978359 then 
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/551e048f7cc4674cb5beecad20960617.lua"))()
+elseif GameID == 3734304510 then
+    loadstring(game:HttpGet("https://api.luarmor.net/files/v3/loaders/27fe72aa1ddbf02b939255b440ace2b3.lua"))()
+end
